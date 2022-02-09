1fc5742317469c5816830d16677d57cac5f59a94
Jeremiah <jeremiah.plauche@gmail.com>
2022-02-09 15:26:30 -0600

    Trying to get that follow ball camera to work, having issues finding a cinemachine setting I like

--- Assets/Scripts/Throw.cs
+++ Assets/Scripts/Throw.cs
@@ -10,50 +10,56 @@ public class Throw : MonoBehaviour
     [SerializeField] float throwPower = 5;
     [SerializeField] Animator cineAnimator;
     [SerializeField] CinemachineVirtualCamera ballVirtualCamera;
+    [SerializeField] GameObject throwSpot;
     public bool followingBall = false;
-    // private InputActions _input;
-    // // StarterAssetsInputs 
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     _input = new InputActions();
-    //     _input.Player.Throw.performed += ctx => ThrowBall();
-    //     if(!cineAnimator)
-    //         cineAnimator = GetComponent<Animator>();
+    private InputActions _input;
+    // StarterAssetsInputs 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _input = new InputActions();
+        _input.Player.Throw.performed += ctx => ThrowBall();
+    }
+    void Start()
+    {
+        // _input = new InputActions();
         
-    // }
-    // private void OnEnable()
-    // {
-    //     _input.Enable();
-    // }
-    // private void OnDisable()
-    // {
-    //     _input.Disable();
-    // }
+        if(!cineAnimator)
+            cineAnimator = GetComponent<Animator>();
+        
+    }
+    private void OnEnable()
+    {
+        _input.Enable();
+    }
+    private void OnDisable()
+    {
+        _input.Disable();
+    }
 
-    // void ThrowBall()
-    // {
-    //     if(!followingBall)
-    //     {
-    //         GameObject ball = Instantiate(ballPrefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-    //         // ball.GetComponent<DodgeballController>();
-    //         Rigidbody ballRb = ball.GetComponent<Rigidbody>();
-    //         ballRb.AddForce(transform.forward * throwPower);
-    //         ballVirtualCamera.LookAt = ball.transform;
-    //         ballVirtualCamera.Follow = ball.transform;
-    //         cineAnimator.SetTrigger("SwitchToBall");
+    void ThrowBall()
+    {
+        if(!followingBall)
+        {
+            GameObject ball = Instantiate(ballPrefab, throwSpot.transform.position, throwSpot.transform.rotation) as GameObject;
+            // ball.GetComponent<DodgeballController>();
+            Rigidbody ballRb = ball.GetComponent<Rigidbody>();
+            ballRb.AddForce(throwSpot.transform.forward * throwPower,ForceMode.VelocityChange);
+            ballVirtualCamera.LookAt = ball.transform;
+            ballVirtualCamera.Follow = ball.transform;
+            cineAnimator.SetTrigger("SwitchToBall");
             
-    //         followingBall = true;
-    //     }
-    //     else
-    //     {
-    //         cineAnimator.SetTrigger("SwitchToPlayer");
-    //         followingBall = false;
-    //     }
-    // }
-    // // Update is called once per frame
-    // void Update()
-    // {
+            followingBall = true;
+        }
+        else
+        {
+            cineAnimator.SetTrigger("SwitchToPlayer");
+            followingBall = false;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
         
-    // }
+    }
 }
