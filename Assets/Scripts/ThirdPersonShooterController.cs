493d8bd7560235859c2b96f38cb774129d875f1a
edjees <elyest98@gmail.com>
2022-02-10 17:39:52 -0500

    first commit, 3rd person camera zoom

---
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using StarterAssets;
+
+public class ThirdPersonShooterController : MonoBehaviour
+{   
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    private StarterAssetsInputs starterAssetsInputs;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+    }
+
+    private void Update()
+    {
+        if(starterAssetsInputs.aim)
+        {
+            aimVirtualCamera.gameObject.SetActive(true);
+        }
+        else{
+            aimVirtualCamera.gameObject.SetActive(false);
+        }
+    }
+}
