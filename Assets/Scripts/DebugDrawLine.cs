85c33d8b601d5d89a93f5f72707e2c558372f084
Jeremiah <jeremiah.plauche@gmail.com>
2022-02-16 14:13:29 -0600

    minor tweak

--- Assets/Scripts/DebugDrawLine.cs
+++ Assets/Scripts/DebugDrawLine.cs
@@ -6,10 +6,13 @@ public class DebugDrawLine : MonoBehaviour
 {
     [SerializeField] Transform lineOrigin;
     [SerializeField] Color gizmoColor = Color.green;
+    [SerializeField] Vector3 eulerOffset = Vector3.zero;
     private void OnDrawGizmos()
     {
         Gizmos.color = gizmoColor;
-        Debug.DrawLine(lineOrigin.position,lineOrigin.position+lineOrigin.forward*50,gizmoColor);
+        Vector3 end = lineOrigin.position+lineOrigin.forward*50;
+
+        Debug.DrawLine(lineOrigin.position,Quaternion.Euler(eulerOffset)*end,gizmoColor);
         // Debug.DrawRay(lineOrigin.position,lineOrigin.forward,gizmoColor,50);
     }
 }
