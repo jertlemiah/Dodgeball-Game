bbd11277aa0765cb4146ef1c8eeff01f29c6fff1
HalpImStuck <aschroeder31@gatech.edu>
2022-03-03 17:37:01 -0500

    Elyes Player Merge Resolution
    I hand merged all Elyes files from his pull request. I had to "re-prefab" the PlayerArmature, there was a few unsaved changes in it.
    I tested it all out on my scene, and it all works good with no errors.
    Only problem is that the Elyes scene is pretty borked at a Unity level, so Elyes you are gonna have to make a new scene.

--- Assets/Scripts/ThirdPersonShooterController.cs
+++ Assets/Scripts/ThirdPersonShooterController.cs
@@ -7,47 +7,66 @@ using StarterAssets;
 public class ThirdPersonShooterController : MonoBehaviour
 {   
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
-    [SerializeField] private float normalSensitivity = 2f;
-    [SerializeField] private float aimSensitivity = 0.5f;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivity;
     [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+
+    [SerializeField] private GameObject handSpot;
     //[SerializeField] private Transform debugTransform;
 
     private StarterAssetsInputs starterAssetsInputs;
     private ThirdPersonController thirdPersonController;
 
     private PickUpZoneController pickUpZoneController;
+    private Animator anim;
 
     private GameObject ball;
     private Rigidbody rb;
-    public float shift_x = 0.4f;
-    public float shift_y = 1.4f;
-    public float shift_z = 0.4f;
-    public float throw_speed = 10f;
+    public float shift_x;
+    public float shift_y;
+    public float shift_z;
+    public float throw_speed;
+
+    private Vector3 mouseWorldPosition;
     // Start is called before the first frame update
     private void Awake()
     {
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
         thirdPersonController = GetComponent<ThirdPersonController>();
         pickUpZoneController = GetComponentInChildren<PickUpZoneController>();
+        anim = GetComponent<Animator>();
+    }
+
+    public void ReleaseBall()
+    {
+        ball.transform.parent = null;
+        rb.isKinematic = false;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        Vector3 throw_direction = (mouseWorldPosition - ball.transform.position).normalized;
+        rb.AddForce(throw_direction*throw_speed*100f);
+        pickUpZoneController.hasBall = false;
+        anim.SetBool("Throw", false);
+    }
+
+    public void PickUpBall()
+    {
+        ball.transform.parent = handSpot.transform;
+        ball.transform.localPosition = Vector3.zero;
+        ball.transform.localRotation = Quaternion.identity;
+
+        rb = ball.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+
+        pickUpZoneController.hasBall = true;
+        pickUpZoneController.foundBall = false;
+
+        anim.SetBool("PickUp", false);
     }
 
     private void Update()
     {   
-        if(pickUpZoneController.foundBall)
-        {   
-            ball = pickUpZoneController.ball.transform.parent.gameObject;
-            ball.transform.position = transform.position + new Vector3(transform.right.x * shift_x, shift_y, transform.right.z * shift_z);
-            rb = ball.GetComponent<Rigidbody>();
-            rb.constraints = RigidbodyConstraints.FreezeAll;
-            pickUpZoneController.hasBall = true;
-            pickUpZoneController.foundBall = false;
-        }
-
-        if(pickUpZoneController.hasBall){
-            ball.transform.position = transform.position + new Vector3(transform.right.x * shift_x, shift_y, transform.right.z * shift_z);
-        }
-
-        Vector3 mouseWorldPosition = Vector3.zero;
 
         Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
         Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
@@ -57,8 +76,20 @@ public class ThirdPersonShooterController : MonoBehaviour
             mouseWorldPosition = raycastHit.point;
         }
 
-        if(starterAssetsInputs.aim)
+        if(pickUpZoneController.foundBall)
+        {   
+            ball = pickUpZoneController.ball.transform.parent.gameObject;
+            anim.SetBool("PickUp", true);
+        }
+
+        if(anim.GetBool("PickUp"))
         {
+            transform.forward = ball.transform.position-transform.position;
+        }
+    
+        if(starterAssetsInputs.aim && pickUpZoneController.hasBall)
+        {   
+            anim.SetBool("Aim", true);
             aimVirtualCamera.gameObject.SetActive(true);
             thirdPersonController.SetSensitivity(aimSensitivity);
             thirdPersonController.SetRotateOnMove(false);
@@ -66,18 +97,16 @@ public class ThirdPersonShooterController : MonoBehaviour
             Vector3 worldAimTarget =  new Vector3(mouseWorldPosition.x, transform.position.y, mouseWorldPosition.z);
             Vector3 aimDirection = (worldAimTarget - transform.position);
             transform.forward = Vector3.Lerp(transform.forward, aimDirection, Time.deltaTime * 20f);
-
-            if(starterAssetsInputs.throw_bool && pickUpZoneController.hasBall){
-                Debug.Log("thrown");
-                rb.constraints = RigidbodyConstraints.None;
-                Vector3 throw_direction = (mouseWorldPosition - ball.transform.position).normalized;
-                rb.AddForce(throw_direction*throw_speed*100f);
-                pickUpZoneController.hasBall = false;
-                GameManager.Instance.TEMP_TurnOffBallHUD();
+             
+            if(starterAssetsInputs.throw_bool){
+                anim.SetBool("Throw", true);
+                //rb.constraints = RigidbodyConstraints.None;
+                
             }
            
         }
         else{
+            anim.SetBool("Aim", false);
             aimVirtualCamera.gameObject.SetActive(false);
             thirdPersonController.SetSensitivity(normalSensitivity);
             thirdPersonController.SetRotateOnMove(true);
