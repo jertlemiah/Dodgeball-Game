ab1e243193f8131a1d8586a90e97de8ea125fa49
Jeremiah <jeremiah.plauche@gmail.com>
2022-03-10 09:36:29 -0600

    Added a temporary Settings screen, needs to be filled in with things

--- Assets/Scripts/Controllers/UI/MainMenuController.cs
+++ Assets/Scripts/Controllers/UI/MainMenuController.cs
@@ -8,7 +8,8 @@ public class MainMenuController : MonoBehaviour
     [SerializeField] GameObject titleScreenGO;
     [SerializeField] GameObject playScreenGO;
     [SerializeField] GameObject settingsScreenGO;
-    [SerializeField] float offscreenOffset = 1000f;
+    [SerializeField] float offscreenHorizontal = 1000f;
+    [SerializeField] float offscreenVertical = 600f;
     [SerializeField] float screenTransitionTime = 1f;
     // Start is called before the first frame update
     void Start()
@@ -23,26 +24,25 @@ public class MainMenuController : MonoBehaviour
     }
     public void Title_PlayButton()
     {
-        titleScreenGO.GetComponent<RectTransform>().DOMoveX(-offscreenOffset,screenTransitionTime);
+        titleScreenGO.GetComponent<RectTransform>().DOMoveX(-offscreenHorizontal,screenTransitionTime);
         playScreenGO.GetComponent<RectTransform>().DOMoveX(0,screenTransitionTime);
-    }
-    public void Title_TrainingButton()
-    {
-        
-    }
-    public void Title_SettingsButton()
-    {
-        
     }
     public void Play_ReturnButton()
     {
         titleScreenGO.GetComponent<RectTransform>().DOMoveX(0,screenTransitionTime);
-        playScreenGO.GetComponent<RectTransform>().DOMoveX(offscreenOffset,screenTransitionTime);
+        playScreenGO.GetComponent<RectTransform>().DOMoveX(offscreenHorizontal,screenTransitionTime);
     }
-    public void Play_LoadLevel(SceneIndex sceneIndex) 
+    public void Title_SettingsButton()
     {
-        GameSceneManager.Instance.LoadScene(sceneIndex);
+        titleScreenGO.GetComponent<RectTransform>().DOMoveY(offscreenVertical,screenTransitionTime);
+        settingsScreenGO.GetComponent<RectTransform>().DOMoveY(0,screenTransitionTime);
     }
+    public void Settings_ReturnButton()
+    {
+        titleScreenGO.GetComponent<RectTransform>().DOMoveY(0,screenTransitionTime);
+        settingsScreenGO.GetComponent<RectTransform>().DOMoveY(-offscreenVertical,screenTransitionTime);
+    }
+    
     public void QuitButton()
     {
         Debug.Log("Quitting game");
