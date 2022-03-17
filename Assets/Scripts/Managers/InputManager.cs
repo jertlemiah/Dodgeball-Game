f064161a25032a3ebe89ba5ac2fbc21bd024a512
Jeremiah <jeremiah.plauche@gmail.com>
2022-03-16 19:20:11 -0500

    Minor tweak to the input manager

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -19,7 +19,7 @@ public class InputManager : Singleton<InputManager>
         _input = new InputActions();
         _input.Player.Pause.performed += context => PausePerformed();
         EventManagerSO.E_PauseGame += EnableMouse;
-        // EventManagerSO.E_StartPrematch += EnableMouse;
+        EventManagerSO.E_FinishedLoading += DisableMouse;
         EventManagerSO.E_EndMatch += EndMatchCleanup;
         EventManagerSO.E_UnpauseGame += DisableMouse;
         DisableMouse();
@@ -34,7 +34,7 @@ public class InputManager : Singleton<InputManager>
         _input.Disable();
         _input.Player.Pause.performed -= context => PausePerformed() ;
         EventManagerSO.E_PauseGame -= EnableMouse;
-        // EventManagerSO.E_StartPrematch -= EnableMouse;
+        EventManagerSO.E_FinishedLoading -= DisableMouse;
         EventManagerSO.E_EndMatch -= EndMatchCleanup;
         EventManagerSO.E_UnpauseGame -= DisableMouse;
     }
